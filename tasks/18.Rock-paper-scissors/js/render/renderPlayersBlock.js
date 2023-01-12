250b5ec7e1c9411aab9296d871c6386b31dfbfe3
stordev <stordbox@gmail.com>
2023-01-12 21:12:01 +0100

    Make code object-oriented: rendering players block

--- tasks/18.Rock-paper-scissors/js/render/renderPlayersBlock.js
+++ tasks/18.Rock-paper-scissors/js/render/renderPlayersBlock.js
@@ -2,66 +2,88 @@ import { playListURL } from '../utils/constants.js';
 import { requestData } from '../utils/requestData.js';
 import { fetchContent } from '../utils/fetchContent.js';
 
-let remainingTime = 600; // seconds
-let playersBlockRendered = false;
-let block = null;
-let intervalId = null;
+class PlayerBlock {
+    constructor(container) {
+        this.remainingTime = 600; // seconds
+        this.playersBlockRendered = false;
+        this.block = null;
+        this.intervalId = null;
+        this.container = container;
+        this.token = window.application.tokens['auth-token'];
+        this.currentYouPlayer = null;
+    }
 
-export function renderPlayersBlockWithTimeout(container) {
-    fetchContent('blocks/lobby.html', '.players', (contentBlock) => {
-        block = contentBlock;
-        if (!playersBlockRendered) {
-            playersBlockRendered = true;
-            renderPlayersBlock(container);
-        }
+    startRendering() {
+        fetchContent('blocks/lobby.html', '.players', (contentBlock) => {
+            this.block = contentBlock;
+            if (!this.playersBlockRendered) {
+                this.playersBlockRendered = true;
+                this.render();
+            }
 
-        intervalId = setInterval(() => {
-            renderPlayersBlock(container);
-        }, 2000);
+            this.intervalId = setInterval(() => {
+                this.render();
+            }, 2000);
 
-        window.application.timers.push(intervalId);
+            window.application.timers.push(this.intervalId);
 
-        setTimeout(() => {
-            clearInterval(intervalId);
-            location.reload();
-        }, remainingTime * 1000);
-    }); // close fetchContent
+            setTimeout(() => {
+                this.stopRendering();
+                location.reload();
+            }, this.remainingTime * 1000);
+        });
+    }
+
+    stopRendering() {
+        clearInterval(this.intervalId);
+    }
+
+    render() {
+        requestData({
+            url: `${playListURL}?token=${this.token}`,
+            callback: (data) => {
+                window.application.players = data.list;
+                this.fillPlayersBlock();
+            }
+        });
+    }
+
+    fillPlayersBlock() {
+        this.container.textContent = '';
+        this.container.appendChild(this.block);
+
+        const players = this.container.querySelector('.players__body');
+        const playerItem = players.querySelector('.players__item');
+        players.textContent = '';
+
+        window.application.players.forEach((player) => {
+            if (player.login === window.application.login) {
+                player.you = true;
+            }
+
+            const clone = playerItem.cloneNode(true);
+            const userName = clone.querySelector('.login');
+            userName.textContent = player.login;
+
+            if (player.you) {
+                if (this.currentYouPlayer) {
+                    this.currentYouPlayer.classList.remove('you');
+                }
+                this.currentYouPlayer = userName;
+                userName.classList.add('you');
+            }
+
+            clone.querySelector('.loses').textContent = player.loses;
+            clone.querySelector('.papers').textContent = player.papers;
+            clone.querySelector('.rocks').textContent = player.rocks;
+            clone.querySelector('.scissors').textContent = player.scissors;
+            clone.querySelector('.wins').textContent = player.wins;
+            players.appendChild(clone);
+        });
+    }
 }
 
-function renderPlayersBlock(container) {
-    const token = window.application.tokens['auth-token']; // get token        
-    requestData({
-        url: `${playListURL}?token=${token}`,
-        callback: (data) => {
-            window.application.players = data.list; // store players            
-            fillPlayersBlock(container);
-        }
-    }); // close requestData function                
-} // close renderPlayersBlock
-
-function fillPlayersBlock(container) {
-    container.textContent = ''; // Remove all children from current content
-    container.appendChild(block); // Add new content
-
-    // ###### fill players table ######
-    const players = container.querySelector('.players__body');
-    const playerItem = players.querySelector('.players__item');
-    players.textContent = ''; // Remove all children from current content
-
-    // clone players
-    const list = window.application.players;
-    list.forEach((player) => {
-        const clone = playerItem.cloneNode(true);
-        const userName = clone.querySelector('.login');
-        userName.textContent = player.login;
-        if (player['you']) {
-            userName.classList.add('you');
-        }
-        clone.querySelector('.loses').textContent = player.loses;
-        clone.querySelector('.papers').textContent = player.papers;
-        clone.querySelector('.rocks').textContent = player.rocks;
-        clone.querySelector('.scissors').textContent = player.scissors;
-        clone.querySelector('.wins').textContent = player.wins;
-        players.appendChild(clone);
-    }); // close forEach        
-} // close fillPlayersBlock
+export function renderPlayersBlockWithTimeout(container) {
+    const playerBlock = new PlayerBlock(container);
+    playerBlock.startRendering();
+}
